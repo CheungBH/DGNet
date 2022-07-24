de99eac6fcebae45608a6b9bfd9ce18e0aaae7c5
cheungbh <534660436@qq.com>
2022-07-24 22:56:19 +0800

    add more cmds

--- train_cmd.py
+++ train_cmd.py
@@ -4,10 +4,14 @@ cmds = [
     "python main.py -d /media/hkuit155/NewDisk/imagenet -dset imagenet -a resdg50 -lr 0.05 --weight-decay 4e-5 --epochs 200 --gpu-id 0 --den-target 0.5 --pretrained pytorch --lr-mode cosine --batch-size 256 --checkpoint imagenet_resnet50/DGNet_50",
     "python main.py -d /media/hkuit155/NewDisk/imagenet -dset imagenet -a resdg50 -lr 0.05 --weight-decay 4e-5 --epochs 200 --gpu-id 0 --den-target 0.5 --pretrained pytorch --lr-mode cosine --batch-size 256 --checkpoint imagenet_resnet50/DPACS_50",
     "python main.py -d /media/hkuit155/NewDisk/imagenet -dset imagenet -a resdg50 -lr 0.05 --weight-decay 4e-5 --epochs 200 --gpu-id 0 --den-target 0.5 --pretrained pytorch --lr-mode cosine --batch-size 256 --checkpoint imagenet_resnet50/DPACS_full_50",
+    "python main.py -d /media/hkuit155/NewDisk/imagenet -dset imagenet -a resdg50 -lr 0.05 --weight-decay 4e-5 --epochs 200 --gpu-id 0 --den-target 0.5 --pretrained pytorch --lr-mode cosine --batch-size 256 --checkpoint imagenet_resnet50/DPACS_full_50_g1 --group_size 1",
+    "python main.py -d /media/hkuit155/NewDisk/imagenet -dset imagenet -a resdg50 -lr 0.05 --weight-decay 4e-5 --epochs 200 --gpu-id 0 --den-target 0.5 --pretrained pytorch --lr-mode cosine --batch-size 256 --checkpoint imagenet_resnet50/DPACS_full_50_g1_dual --group_size 1 --dual_fc",
 
     "python main.py -d /media/hkuit155/NewDisk/imagenet -dset imagenet -a mobilenet_v2_dg -lr 0.05 --weight-decay 4e-5 --epochs 200 --gpu-id 0 --den-target 0.5 --pretrained pytorch --lr-mode cosine --batch-size 256 --checkpoint imagenet_mobile/DGNet_50",
     "python main.py -d /media/hkuit155/NewDisk/imagenet -dset imagenet -a mobilenet_v2_dg -lr 0.05 --weight-decay 4e-5 --epochs 200 --gpu-id 0 --den-target 0.5 --pretrained pytorch --lr-mode cosine --batch-size 256 --checkpoint imagenet_mobile/DPACS_50",
     "python main.py -d /media/hkuit155/NewDisk/imagenet -dset imagenet -a mobilenet_v2_dg -lr 0.05 --weight-decay 4e-5 --epochs 200 --gpu-id 0 --den-target 0.5 --pretrained pytorch --lr-mode cosine --batch-size 256 --checkpoint imagenet_mobile/DPACS_full_50",
+    "python main.py -d /media/hkuit155/NewDisk/imagenet -dset imagenet -a mobilenet_v2_dg -lr 0.05 --weight-decay 4e-5 --epochs 200 --gpu-id 0 --den-target 0.5 --pretrained pytorch --lr-mode cosine --batch-size 256 --checkpoint imagenet_mobile/DPACS_full_50_g1 --group_size 1",
+    "python main.py -d /media/hkuit155/NewDisk/imagenet -dset imagenet -a mobilenet_v2_dg -lr 0.05 --weight-decay 4e-5 --epochs 200 --gpu-id 0 --den-target 0.5 --pretrained pytorch --lr-mode cosine --batch-size 256 --checkpoint imagenet_mobile/DPACS_full_50_g1_dual --group_size 1 --dual_fc",
 ]
 
 import os
